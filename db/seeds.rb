4d72382d8ced3e019ff7f53123527ed299b32094
dobecma <dobeckmax@gmail.com>
2017-09-12 19:40:29 -0700

    Test worked and two separate users could see different things depending on their role.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create([{ email: 'frank@paddys.com', password: 'p'}, { email: 'dennis@paddys.com', password: 'p' }])
+users = User.create([{ email: 'frank@paddys.com', password: 'p', supervisor: true, admin: true}, { email: 'dennis@paddys.com', password: 'p' }])
