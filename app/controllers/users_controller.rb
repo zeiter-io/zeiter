660d94b4824982be8dc8da0fbbb69532265ea2b2
Max Dobeck <dobeckmax@gmail.com>
2017-09-17 18:19:37 -0700

    Added privat params to Users controller and changed back link to Home link_to on users show page. (#20)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
