16e571764c29331ff7796dbe4d083aca3e1f149c
dobecma <dobeckmax@gmail.com>
2017-09-17 18:36:10 -0700

    Fixed the seed file for updated Role model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create([{ email: 'frank@paddys.com', password: 'p', supervisor: true, admin: true}, { email: 'dennis@paddys.com', password: 'p', god: true }, { email: 'mac@paddys.com', password: 'p' }, { email: 'dee@paddys.com', password: 'p' }, { email: 'charlie@paddys.com', password: 'p' }])
+users = User.create([{ email: 'frank@paddys.com', password: 'p' }, { email: 'dennis@paddys.com', password: 'p', }, { email: 'mac@paddys.com', password: 'p' }, { email: 'dee@paddys.com', password: 'p' }, { email: 'charlie@paddys.com', password: 'p' }])
