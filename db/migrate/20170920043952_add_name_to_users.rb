8518d69bdd64b07ae1a3b9513e2151287f4ca72c
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-09-19 23:34:26 -0700

    Fix typo in User migration to add name.

--- db/migrate/20170920043952_add_name_to_users.rb
+++ db/migrate/20170920043952_add_name_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :first_name, :string
-    add_column :users, :middle_name, :stringg
+    add_column :users, :middle_name, :string
     add_column :users, :last_name, :string
 
     add_index :users, :first_name
