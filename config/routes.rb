73f868364c6c3119806034c02ef2c87a4f3acd96
dobecma <dobeckmax@gmail.com>
2017-09-22 01:25:04 -0700

    Unnested the users and schedules routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   get 'users/index'
-  resources :users 
+  resources :users
   resources :schedules
   resources :schedules_users
   root 'home#index'  
