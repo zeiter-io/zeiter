8df2a7181dcf64a9e5ccf2bf7733667f6dfcd574
kapoorlakshya <kapoorlakshya@gmail.com>
2017-10-03 00:02:31 -0700

    Add swappable attribute to Shifts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002073405) do
+ActiveRecord::Schema.define(version: 20171003070149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171002073405) do
     t.datetime "repeat_end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "swappable"
   end
 
   create_table "users", force: :cascade do |t|
