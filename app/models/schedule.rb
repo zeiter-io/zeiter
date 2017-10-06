90781c568860b47f8ceb86a0b0a646e47f772d4a
dobecma <dobeckmax@gmail.com>
2017-10-05 22:09:58 -0700

    Changed the code check == nil operation to ! code check to rubify.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -12,13 +12,13 @@ class Schedule < ApplicationRecord
     char = ('a'..'z').to_a; num = ('1'..'9').to_a
     new_special_code = 5.times.map{ [ *char, *num ].sample }.join
     code_check = Schedule.find_by_special_code(new_special_code)
-    if code_check == nil
+    if !code_check
       self.special_code = new_special_code
       return
     else 
       while code_check != nil
-      new_special_code = 5.times.map{ [ *char, *num ].sample }.join
-      code_check = Schedule.find_by_special_code(new_special_code)
+        new_special_code = 5.times.map{ [ *char, *num ].sample }.join
+        code_check = Schedule.find_by_special_code(new_special_code)
       end
       self.special_code = new_special_code
       return
