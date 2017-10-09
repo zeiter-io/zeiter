cdc9c13105b3b961264e6741ded47d208b391ed2
dobecma <dobeckmax@gmail.com>
2017-10-08 22:03:20 -0700

    Added first_name to seed file and making sure it shows up.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 users = User.create([{ email: 'frank@paddys.com', password: 'p' }, 
-{ email: 'dennis@paddys.com', password: 'p', }, 
+{ first_name: 'Dennis', email: 'dennis@paddys.com', password: 'p', }, 
 { email: 'mac@paddys.com', password: 'p' }, 
 { email: 'dee@paddys.com', password: 'p' }, 
 { email: 'charlie@paddys.com', password: 'p' }])
