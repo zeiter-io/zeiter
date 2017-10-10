3800e799b7200aaaa10942b6feba412bb6dea9e6
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-10-10 00:08:39 -0700

    Add User ID col to Shifts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004053428) do
+ActiveRecord::Schema.define(version: 20171010070702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20171004053428) do
     t.boolean "swappable"
     t.integer "shift_id"
     t.integer "clone_of"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
