737e11cc674bba7bacd7c63ce8b074ebc0979521
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-10-22 23:55:50 -0700

    Add back missing routes and reformat.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   resources :shifts
   devise_for :users
 
-  # Global Search - Typeahead drop down
+  get 'home/index'
+  get 'users/index'
+
+  resources :schedules do
+    resources :enrollments, shallow: true, except: :index
+  end
+  get 'schedules/:id/enrollments', to: redirect('/schedules/%{id}')
+
+  # Global Search - Typeahead feature
   resources :users do
     collection do
       get :typeahead
     end
   end
-
-  resources :schedules do
-    resources :enrollments, shallow: true, except: :index
-  end
-
-  get 'schedules/:id/enrollments', to: redirect('/schedules/%{id}')
 
   root 'home#index'
 end
